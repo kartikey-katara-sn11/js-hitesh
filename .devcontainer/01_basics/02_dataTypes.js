ed84c6810b6bc95f8b7c2f6a4a313a21f28c7937
Kartikey Katara <113904708+kartikey-katara-sn11@users.noreply.github.com>
2025-02-19 12:31:03 +0000

    basics

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -6,3 +6,17 @@
  
  console.log("JS")
  
+
+// number => 2 to 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
