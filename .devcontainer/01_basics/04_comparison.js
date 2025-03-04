2e16e60dbd959d9a4688743c02378e16a0540229
Kartikey Katara <113904708+kartikey-katara-sn11@users.noreply.github.com>
2025-03-04 02:37:00 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/*
+most of the cases, we avoide to create this kind of conversions
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+*/
+
+// ===  Strictly Check
+
+console.log("2" === 2);
