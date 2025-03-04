723068b18c350bbbef9569fcab82ccea73ffc866
Kartikey Katara <113904708+kartikey-katara-sn11@users.noreply.github.com>
2025-03-04 02:37:44 +0000

    #comaparison

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -15,11 +15,37 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1  // "" => false  || "Kunj" => true
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);  // Output= true
+// console.log(booleanIsLoggedIn);  // Output= true
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+
+// ******************************************** Operations **********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = "Hello"
+let str2 = " Kunj"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
