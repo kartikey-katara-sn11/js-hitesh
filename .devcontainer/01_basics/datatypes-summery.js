c5545361d260461ddd1767c9f9caf225fed89910
Kartikey Katara <113904708+kartikey-katara-sn11@users.noreply.github.com>
2025-03-05 12:09:38 +0000

    datatypes summery

--- .devcontainer/01_basics/datatypes-summery.js
+++ .devcontainer/01_basics/datatypes-summery.js
@@ -34,3 +34,37 @@ const myFunction = function(){
 }
 
 console.log(typeof bigNumber);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Priitive), Heap(Non primitive)
+
+let myName = "Kunj Sharma"
+
+let anotherName = myName
+anotherName = "Kartikey"
+
+
+console.log(myName);
+console.log(anotherName);
+
+
+
+
+let userOne = {
+    email : "user1@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user2@gmai.com"
+
+
+console.log(userOne);
+console.log(userTwo);
